19e72e17308df70bd6f58d234ba780b3f0ed6cda
Julia Yerman <74793435+yermanjulia@users.noreply.github.com>
2024-05-09 21:54:49 -0700

    Timeouts

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
   await page.goto("http://www.uitestingplayground.com/ajax/");
   await page.getByText("Button Triggering AJAX Request").click();
+  testInfo.setTimeout(testInfo.timeout + 2000);
 });
 
 test("auto waiting", async ({ page }) => {
@@ -20,23 +21,29 @@ test("auto waiting", async ({ page }) => {
   await expect(successButton).toHaveText("Data loaded with AJAX get request.", {
     timeout: 20000,
   });
+});
 
-  test("alternative waits", async ({ page }) => {
-    const successButton = page.locator(".bg-success");
+test("alternative waits", async ({ page }) => {
+  const successButton = page.locator(".bg-success");
 
-    //---wait for element
-    // await page.waitForSelector("");
+  //---wait for element
+  // await page.waitForSelector("");
 
-    //--- wait for particular response
-    // await page.waitForResponse("http://www.uitestingplayground.com/ajaxdata");
+  //--- wait for particular response
+  // await page.waitForResponse("http://www.uitestingplayground.com/ajaxdata");
 
-    //-----wait for network calls to be completed ('NOT RECOMMENDED')
-    await page.waitForLoadState("networkidle");
+  //-----wait for network calls to be completed ('NOT RECOMMENDED')
+  await page.waitForLoadState("networkidle");
 
-    // await page.waitForURL
+  // await page.waitForURL
 
-    const text = await successButton.allTextContents();
+  const text = await successButton.allTextContents();
 
-    expect(text).toContain("Data loaded with AJAX get request.");
-  });
+  expect(text).toContain("Data loaded with AJAX get request.");
+});
+
+test("timeouts", async ({ page }) => {
+  test.setTimeout(20000);
+  const successButton = page.locator(".bg-success");
+  await successButton.click();
 });
